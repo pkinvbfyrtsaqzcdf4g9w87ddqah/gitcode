f36c8497283195620bd0194eb9eac85d04779e3f
pkinvbfyrtsaqzcdf4g9w87ddqah <u0452009@nkfust.edu.tw>
2018-05-18 11:14:18 +0800

    third commit

--- main.c
+++ main.c
@@ -4,10 +4,10 @@
 
 /* run this program using the console pauser or add your own getch, system("pause") or input loop */
 
-int main(int argc, char *argv[]) {
+int main(int argc, char *argv[]) {  //無返回值類型
 	float r,area;//定義兩個浮點型變量
 	printf("輸入半徑：");//友情提示
-	scanf("%f",&r);
-	area=PI*r*r;
-	printf("面積為：%f",area);
+	scanf("%f",&r);//接收用戶輸入數據
+	area=PI*r*r; //開始計算
+	printf("面積為：%f",area);//得出結果
 }
